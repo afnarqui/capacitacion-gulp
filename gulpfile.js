aecd576f37f3fda59e4993c8e17891e580fea427
julio naranjo <juliocesarn2010@hotmail.com>
2016-05-06 09:56:16 -0500

    3 crear servicio2 con ruta especificada

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
+
+ // crea servicio con la ruta especificada
+
 var gulp = require('gulp'),
  connect = require('gulp-connect');
 
- gulp.task('connect1', function(){
-  connect.server();
+
+ gulp.task('connect2', function(){
+  connect.server({
+    root: 'app/source/templates/'
+  });
  });
 
- gulp.task('default', ['connect1']);
+ gulp.task('default',['connect']);
