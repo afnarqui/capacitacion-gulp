1cece738e94376890b8942230e959efb6d6c540d
julio naranjo <juliocesarn2010@hotmail.com>
2016-05-06 10:06:52 -0500

    4 crea servicio3 con parametros y livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 
- // crea servicio con la ruta especificada
+// crea servicio con la ruta especificada,livereload,watch y puerto
 
 var gulp = require('gulp'),
  connect = require('gulp-connect');
 
 
- gulp.task('connect2', function(){
+ gulp.task('connect3', function(){
   connect.server({
-    root: 'app/source/templates/'
+    root: 'app/source/templates/',
+    port: 3333,
+    livereload: true
   });
  });
 
- gulp.task('default',['connect']);
+ gulp.task('reloadHtml', function(){
+  gulp.src('./app/source/templates/*.html')
+  .pipe(connect.reload());
+ });
+
+ gulp.task('watch', function(){
+  gulp.watch(['./app/source/templates/*.html'],['reloadHtml']);
+ });
+
+ gulp.task('default',['connect3','watch']);
