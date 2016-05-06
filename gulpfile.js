39190c2d792027433483fd3380f91c45e875f7ee
julio naranjo <juliocesarn2010@hotmail.com>
2016-05-06 17:37:04 -0500

    26 crea archivo .json con localstorage

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
-// junta varias imagenes para mejorar rendimiento
-
+// crea un json apartir de una tarea
+ 
 var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
 
-gulp.task('sprite', function () {
-  var spriteData = gulp.src('./app/source/imagenes/*.jpg')
-  .pipe(spritesmith({
-    imgName: 'sprite.jpg',
-    cssName: 'sprite.css'
-  }));
-  return spriteData.pipe(gulp.dest('./out/'));
+// Do this
+var storage = require('gulp-storage')(gulp);
+
+// or do this
+//require('gulp-storage')(gulp);
+
+gulp.storage.create('myStorage', 'usuarios.json');
+
+
+gulp.task('ingresarUsuarios', function(){
+    this.storage.set('usuarios', [
+                { "nombre":"carlos","edad":23,"direccion":"calle 3"},
+                { "nombre":"sergio","edad":33,"direccion":"calle 10"},
+                { "nombre":"andres","edad":66,"direccion":"calle 38"},
+                { "nombre":"miguel","edad":21,"direccion":"calle 31"}
+            ]);
 });
 
 
-   gulp.task('default',['sprite']);
+//  Create a storage.clear(); // Delete everything
+//  Create a storage.delete( key ); // Delete a specific property
+
+   gulp.task('default',['ingresarUsuarios'], function(){
+
+        var appName = this.storage.get('usuarios');
+        console.log(appName)
+        //=> myApp
+
+   });
