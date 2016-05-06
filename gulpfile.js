3a00b0d0b86091a113c535675607deaa03c1bbb9
julio naranjo <juliocesarn2010@hotmail.com>
2016-05-06 17:23:59 -0500

    25 hacer sprite a las imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,16 @@
-const gulp = require('gulp');
-const zip = require('gulp-zip');
-const del = require('del');
-const decompress = require('gulp-decompress');
-const gulpSequence = require('gulp-sequence');
+// junta varias imagenes para mejorar rendimiento
 
-gulp.task('zip', () => {
-    return gulp.src('./app/source/js/*.js')
-        .pipe(zip('archive.zip'))
-        .pipe(gulp.dest('dist/comprimido/'));
+var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('./app/source/imagenes/*.jpg')
+  .pipe(spritesmith({
+    imgName: 'sprite.jpg',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('./out/'));
 });
 
 
-gulp.task('decompress', function () {
-    return gulp.src('./dist/comprimido/*.{tar,tar.bz2,tar.gz,zip}')
-        .pipe(decompress({strip: 1}))
-        .pipe(gulp.dest('./dist/descomprimido/'))
-        //.pipe(del('./dist/*.{tar,tar.bz2,tar.gz,zip}'))
-        //.pipe(dest('./dist/'))
-});
-
-
-  gulp.task('dele', function(){
-    return del('./dist/comprimido/*.{tar,tar.bz2,tar.gz,zip}')
-  });
-
-  gulp.task('deletedir', function(){
-    return del('./dist')
-  });
-
-
-
-
-gulp.task('all', gulpSequence(['deletedir'],['zip'],['decompress']));
-
-
-   gulp.task('default',['all']);
+   gulp.task('default',['sprite']);
