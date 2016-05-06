8c685990019ccf92f507da0aacc820e6e6fbc681
julio naranjo <juliocesarn2010@hotmail.com>
2016-05-06 15:02:48 -0500

    16 hacer back up de archivos y eliminar carpeta sobrante

--- app/source/gulpfiles/install(1).js
+++
@@ -1,9 +0,0 @@
-// install dependencias encontradas en bower.json o package.json
-
-var install = require("gulp-install"),
-       gulp = require('gulp');
-
-gulp.task('install', function(){
-	gulp.src(['./bower.json', './package.json'])
-  	 .pipe(install());
-});
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,35 @@
 // crea un back up 
+// 
+// 
+// despues de crear el back up usar el 
+// plugin de eliminar y borrar la carpeta vacia
+// que queda es un error del plugin
 
 var gulp = require('gulp'),
-  safe = require('gulp-safe');
+  safe = require('gulp-safe'),
+  del = require('del');
  
 gulp.task('backup', function() {
   var dest;
  
-  dest = "./backup";
+  dest = "./";
  
-  return gulp.src("./app/source/gulpfiles/*js")
+  return gulp.src("./app/source/gulpfiles/")
     .pipe(safe(dest))
     .pipe(gulp.dest(dest));
 });
 
-gulp.task('default', ['backup']);
+
+//delete dir and files
+
+
+gulp.task('clean:gulpfiles', function () {
+  return del([
+    './gulpfiles'
+  ]);
+});
+
+
+gulp.task('default', ['backup'], function(){
+    gulp.start('clean:gulpfiles');
+});
